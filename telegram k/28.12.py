66e09be81971047b7402d219a224589ad12d7c50
Al3xiv3 <actionheavy@gmail.com>
2022-12-28 07:55:35 +0300

    комит плз

--- telegram k/28.12.py
+++ telegram k/28.12.py
@@ -1,7 +1,5 @@
-s = "1231212cat123123213cqt123123cat1323132cat"
-с = 0
+t = {3, 2, 5, 1, 2, 1, 2, 3, 4, 5, 6 ,7, 5}
+i = 1
 
-for i in range(0, len(s) - 2):
-    if s[i] + s[i + 1] + s[i + 2] == "cat":
-        с += 1
-print(с)
+while i < 10:
+    print(t)
